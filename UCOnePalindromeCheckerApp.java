bd3994d62d48ad7bd11d48a1cd1305287e1995f9
tiwariagam-1611 <tiwariagam16112003@gmail.com>
2026-03-03 16:08:50 +0530

    Coded UC1 to Initialize App with message

---
+++ UCOnePalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+package com.palindromeapp;
+/*
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ *
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Developer
+ * @version 1.0
+*/
+
+public class UCOnePalindromeCheckerApp {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to the Palindrome Checker Management System\n"
+				+ "Version : 1.0\n"
+				+ "System initialized successfully");
+	}
+}
+
+
